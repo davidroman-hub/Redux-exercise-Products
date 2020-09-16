294801d46b105fd8ced29ddb00e245e3dd69c2d7
David <jobroman83@gmail.com>
2020-09-16 10:44:40 +0200

    Task7 add product to state and admin the error

--- src/components/actions/productActions.js
+++ src/components/actions/productActions.js
@@ -12,8 +12,40 @@ import {
 /// Create new production
 
 export function createNewProductAction (product){
-    return () => {
-        console.log('desde action')
-        console.log(product)
+    return (dispatch) => {
+        // console.log('desde action')
+        // console.log(product)
+
+        dispatch(addProduct() );
+
+        //consult database
+        try{
+            dispatch(addProductSuccess(product))
+        }catch(error){
+            // dispatch execute the the actions (functions)
+            dispatch(addProductError(true))
+        }
     }
 }
+
+const addProduct = () => ({
+    //types descriptions of your application
+
+    type:ADD_PRODUCT,
+    // paylod modify the state
+    payload:true
+})
+
+// if the product save is success in the data base
+
+const addProductSuccess = product => ({
+    type:ADD_PRODUCT_SUCCESS,
+    payload:product
+})
+
+// if is a error
+
+const addProductError = state => ({
+    type:ADD_PRODUCT_ERROR,
+    payload:state
+})
